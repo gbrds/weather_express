deafe0be925ade0e81b9be610126f5844ab7a28f
Juss <juss-joosep.holdt@voco.ee>
2024-11-29 13:17:16 +0200

    create form request response with API data

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 const key = '82ec2ecd496644610e4d031cff77967a' // API key
 let city = 'Tartu'
 
@@ -26,6 +29,24 @@ app.get('/', (req, res) => {
         console.log(temp)
         
         res.render('./index', {
+            description: description,
+            city: city,
+            temp: temp
+        })
+    })
+})
+
+app.post('/', (req, res) => {
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)-273.15)
+        res.render('index', {
             description: description,
             city: city,
             temp: temp
