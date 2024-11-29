7dc55d879f572cf567d2ccc8eb947aa3cd3d168a
Juss <juss-joosep.holdt@voco.ee>
2024-11-29 13:04:14 +0200

    Weather API data add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const { deserialize } = require('v8')
 
 const app = express()
 
@@ -8,8 +9,28 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
 
+const key = '82ec2ecd496644610e4d031cff77967a' // API key
+let city = 'Tartu'
+
 app.get('/', (req, res) => {
-    res.render('index')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)-273.15)
+        console.log(description)
+        console.log(city)
+        console.log(temp)
+        
+        res.render('./index', {
+            description: description,
+            city: city,
+            temp: temp
+        })
+    })
 })
 
 app.listen(3002)
